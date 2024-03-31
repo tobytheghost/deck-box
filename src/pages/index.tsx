dfe10b57a011c78320673dbd215fdaeecbda96a2
Toby <tobytheghost@hotmail.com>
2024-03-31 22:16:36 +0100

    Update landing page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,44 +1,15 @@
-import { useUser, UserButton, SignInButton, SignUpButton } from "@clerk/nextjs";
-
+/* eslint-disable react/no-unescaped-entities */
 export default function Home() {
-  const { isSignedIn, isLoaded, user } = useUser();
   return (
-    <>
-      <Header />
-      {!isLoaded ? (
-        <></>
-      ) : (
-        <main className="">
-          <div className="">
-            {isSignedIn ? (
-              <>
-                <div className="">Welcome {user.firstName}!</div>
-              </>
-            ) : (
-              <div className="">Sign in to create your todo list!</div>
-            )}
-          </div>
-        </main>
-      )}
-    </>
+    <main className="">
+      <div className="flex min-h-screen flex-col items-center justify-center bg-gray-900">
+        <h1 className="mb-8 animate-pulse text-5xl font-bold text-white">
+          Coming Soon
+        </h1>
+        <p className="mb-8 text-lg text-white">
+          We're working hard to bring you something amazing. Stay tuned!
+        </p>
+      </div>
+    </main>
   );
 }
-
-const Header = () => {
-  const { isSignedIn } = useUser();
-
-  return (
-    <header className="">
-      <div>My Todo App</div>
-      {isSignedIn ? (
-        <UserButton />
-      ) : (
-        <div>
-          <SignInButton />
-          &nbsp;
-          <SignUpButton />
-        </div>
-      )}
-    </header>
-  );
-};
